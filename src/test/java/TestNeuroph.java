d60f9e74aeef22ee9ab621f3915b9daf54113b2b
Swapneel Datta <swd543@gmail.com>
2020-03-05 16:34:39 +0100

    Added test

--- src/test/java/TestNeuroph.java
+++ src/test/java/TestNeuroph.java
@@ -1,11 +1,8 @@
 import org.junit.jupiter.api.Test;
-import org.neuroph.core.Layer;
-import org.neuroph.core.Neuron;
 
 public class TestNeuroph {
     @Test
     public void testNeuroph(){
-        var layer=new Layer();
-        layer.addNeuron(new Neuron());
+        System.out.println("Hello");
     }
 }
