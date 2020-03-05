e976e5a25afc8bbbe464fad0ee65458a89f743a6
Swapneel Datta <swd543@gmail.com>
2020-03-05 16:28:23 +0100

    put

---
+++ src/test/java/TestNeuroph.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import org.neuroph.core.Layer;
+import org.neuroph.core.Neuron;
+
+public class TestNeuroph {
+    @Test
+    public void testNeuroph(){
+        var layer=new Layer();
+        layer.addNeuron(new Neuron());
+    }
+}
