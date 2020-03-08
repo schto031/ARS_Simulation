34bc5e775a7916fcaea4e5950b1cd8586b656492
Swapneel Datta <swd543@gmail.com>
2020-03-08 21:47:46 +0100

    Added evaluation

---
+++ src/main/java/Evaluation.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Evaluation {
+    public static List<Robo> getBestBots(Robo[] robots, int number){
+        return Arrays.stream(robots).sorted(Comparator.comparingInt(Robo::getCollectedDust)).limit(number).collect(Collectors.toUnmodifiableList());
+    }
+}
