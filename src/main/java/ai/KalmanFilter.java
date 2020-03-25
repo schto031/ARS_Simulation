2f36dd11db1ff26b89d1fbb50e5c5f8c49eea4b4
buga <swd543@gmail.com>
2020-03-25 04:54:31 +0100

    Added time step

--- src/main/java/ai/KalmanFilter.java
+++ src/main/java/ai/KalmanFilter.java
@@ -9,6 +9,8 @@ public class KalmanFilter {
     // x, y, phi
     private final int numberOfStateVariables=3;
     SimpleMatrix X,Control,A,C,Q,R,covariance;
+    long previousTime=System.currentTimeMillis();
+    double dt=0;
 
     public KalmanFilter(Coordinate.Double initialPosition, double initialOrientation) {
         X=new SimpleMatrix(1, numberOfStateVariables, false, initialPosition.x, initialPosition.y, initialOrientation);
@@ -19,5 +21,11 @@ public class KalmanFilter {
         R=SimpleMatrix.identity(numberOfStateVariables);
         Q=new SimpleMatrix(numberOfStateVariables, numberOfStateVariables);
         Q.plus(0.1);
+    }
+
+    public void update(){
+        var curTime=System.currentTimeMillis();
+        dt=curTime-previousTime;
+        previousTime=curTime;
     }
 }
